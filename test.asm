f01088d8f146a99dbafa7119fff790236664b6c5
Noureddine-0 <114105927+Noureddine-0@users.noreply.github.com>
2023-09-05 01:08:39 -0400

    Create test.asm

---
+++ test.asm
@@ -0,0 +1,20 @@
+;nasm -felf64 -o test.o test.asm
+;ld -o test test.o
+
+;./my_program ./test
+section .text
+global _start
+
+_start:
+
+    mov rdi,0x0065742f706d742f
+    mov rsi,1
+    mov rdx , 0
+    push rdi
+    push rsp
+    pop rdi
+    mov rax,2
+    syscall
+    mov rax ,60
+    mov rdi,0
+    syscall
